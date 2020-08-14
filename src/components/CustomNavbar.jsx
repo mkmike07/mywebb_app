51c84f6ce8062a596704c10a662f060798b5bd23
Mike K Mutabazi <mutamike07@gmail.com>
2020-08-14 16:28:55 -0700

    New changes to NavBar building

--- src/components/CustomNavbar.jsx
+++ src/components/CustomNavbar.jsx
@@ -15,6 +15,17 @@ export default class CustomNavbar extends Component {
                         <Navbar.Brand>
                             <Link to = "/">codeLife</Link>
                         </Navbar.Brand>
+                        <Navbar.Toggle/>
+                        <Navbar.Collapse>
+                            <Nav pullRight>
+                                <NavItem eventKey = {1} componentClass = {Link} to = "/">Home
+                                </NavItem>
+                                <NavItem eventKey = {2} componentClass = {Link} to = "/">About
+                                </NavItem>
+                                <NavItem eventKey = {3} componentClass = {Link} to = "/">News
+                                </NavItem>
+                            </Nav>
+                        </Navbar.Collapse>
                     </Navbar.Header>
                 </Navbar>
             </div>
